33d0b65e2159e4aa648018c463999026dbcfb398
Ayesha <aashipu02@gmail.com>
2021-11-07 21:41:31 +0500

    Add activity 5 and 6

--- app/src/main/java/com/example/arabic/MainActivity2.java
+++ app/src/main/java/com/example/arabic/MainActivity2.java
@@ -10,13 +10,14 @@ import android.widget.Button;
 public class MainActivity2 extends AppCompatActivity {
 Button b;
 Button b11;
+Button b12;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         b = (Button) findViewById(R.id.b1);
         b11 = (Button) findViewById(R.id.b2);
-
+        b12 = (Button) findViewById(R.id.b3);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,6 +30,14 @@ Button b11;
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity2.this, MainActivity4.class);
+                startActivity(intent);
+
+            }
+        });
+        b12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity5.class);
                 startActivity(intent);
 
             }
