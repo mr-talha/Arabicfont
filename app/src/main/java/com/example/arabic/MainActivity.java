41c25d92cb3feb73c07e6e1d56579d06b8bdebc6
Ayesha <aashipu02@gmail.com>
2021-11-07 16:04:07 +0500

    2nd activity

--- app/src/main/java/com/example/arabic/MainActivity.java
+++ app/src/main/java/com/example/arabic/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.arabic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b;
+    Button nxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b=(Button) findViewById(R.id.rl);
+        nxt=(Button)findViewById(R.id.next);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getUrl("https://github.com/aishavlog/BCSF18M040_Arabic_Letter.git");
+            }
+        });
+        nxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+    private void getUrl(String s)
+    {
+        Uri uri=Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
     }
 }
