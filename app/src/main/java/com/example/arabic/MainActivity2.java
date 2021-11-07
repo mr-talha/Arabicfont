b198c668448582f49ba2a74e7a5d5dd2e3689d52
Ayesha <aashipu02@gmail.com>
2021-11-07 20:46:22 +0500

    Add activity 4

--- app/src/main/java/com/example/arabic/MainActivity2.java
+++ app/src/main/java/com/example/arabic/MainActivity2.java
@@ -9,17 +9,26 @@ import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 Button b;
+Button b11;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         b = (Button) findViewById(R.id.b1);
-
+        b11 = (Button) findViewById(R.id.b2);
 
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+
+            }
+        });
+        b11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity4.class);
                 startActivity(intent);
 
             }
