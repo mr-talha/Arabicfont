a865dbd7c61f2dea799d5f3e4a8c2d6a59c9a1bd
Ayesha <aashipu02@gmail.com>
2021-11-07 17:21:25 +0500

    3rd activity

--- app/src/main/java/com/example/arabic/MainActivity2.java
+++ app/src/main/java/com/example/arabic/MainActivity2.java
@@ -2,13 +2,27 @@ package com.example.arabic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
-
+Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        b = (Button) findViewById(R.id.b1);
+
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
