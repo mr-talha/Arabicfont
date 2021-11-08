4096b0b326345c8ccc9f3fb66e10e1d4c54c35d7
Ayesha <aashipu02@gmail.com>
2021-11-08 13:55:15 +0500

    add 2 more activities

--- app/src/main/java/com/example/arabic/MainActivity13.java
+++ app/src/main/java/com/example/arabic/MainActivity13.java
@@ -2,13 +2,25 @@ package com.example.arabic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity13 extends AppCompatActivity {
-
+Button c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main13);
+        c=(Button)findViewById(R.id.button2);
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity13.this, MainActivity12.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
